00fe5ee6d1170585f4e02adcd12302bd588bf930
Adeesh Agrawal <152026656+AlTrUiStIcPoTtErHeAd@users.noreply.github.com>
2025-08-26 15:12:09 +0530

    Update divisible_by_7_or_last_digit_5.py

--- Sheet-1/divisible_by_7_or_last_digit_5.py
+++ Sheet-1/divisible_by_7_or_last_digit_5.py
@@ -1,13 +1,5 @@
 num = int(input("Enter a number: "))
-last_digit = abs(num) % 10
-divisible_by_7 = num % 7 == 0
-if divisible_by_7 or last_digit == 5:
-    print(f"{num} meets at least one condition:")
-    if divisible_by_7:
-        print(f"  - {num} is divisible by 7")
-    if last_digit == 5:
-        print(f"  - Last digit is 5")
+if num % 7 == 0 or num % 10 == 5:
+    print("ok")
 else:
-    print(f"{num} does not meet either condition")
-    print(f"  - Not divisible by 7")
-    print(f"  - Last digit is {last_digit}, not 5")
+    print("not ok")
